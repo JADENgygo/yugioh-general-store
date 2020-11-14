62ac97193a8caf449e168f4f21ae3a8bd3c9a72c
JADEN <oJADENo@users.noreply.github.com>
2020-11-14 22:32:02 +0900

    target blankを消去

--- scripts/script.js
+++ scripts/script.js
@@ -751,22 +751,22 @@ document.addEventListener('DOMContentLoaded', () => {
 						<tr><th>Webサイト</th></tr>
 					</thead>
 					<tbody>
-						<tr><td><a class="uk-link-muted" href="https://www.tv-tokyo.co.jp/anime/yugioh2000/" target="_blank">テレビ東京・あにてれ 遊☆戯☆王 デュエルモンスターズ</a></td></tr>
-						<tr><td><a class="uk-link-muted" href="https://www.tv-tokyo.co.jp/anime/yugioh-dm/" target="_blank">遊☆戯☆王デュエルモンスターズ 20thリマスター テレビ東京アニメ公式</a></td></tr>
-						<tr><td><a class="uk-link-muted" href="https://www.tv-tokyo.co.jp/anime/yugioh2004/" target="_blank">テレビ東京・あにてれ 遊☆戯☆王デュエルモンスターズGX</a></td></tr>
-						<tr><td><a class="uk-link-muted" href="https://www.tv-tokyo.co.jp/anime/yugioh/" target="_blank">遊☆戯☆王ファイブディーズ - テレビ東京</a></td></tr>
-						<tr><td><a class="uk-link-muted" href="https://www.tv-tokyo.co.jp/anime/yugioh-zexal/" target="_blank">遊☆戯☆王ゼアルⅡ：あにてれ：テレビ東京</a></td></tr>
-						<tr><td><a class="uk-link-muted" href="https://www.tv-tokyo.co.jp/anime/yugioh-arcv/" target="_blank">テレビ東京・あにてれ 遊☆戯☆王ARC-V</a></td></tr>
-						<tr><td><a class="uk-link-muted" href="https://www.tv-tokyo.co.jp/anime/yugioh-vrains/" target="_blank">テレビ東京・あにてれ 遊☆戯☆王VRAINS</a></td></tr>
-						<tr><td><a class="uk-link-muted" href="https://www.tv-tokyo.co.jp/anime/yugioh-sevens/" target="_blank">遊☆戯☆王SEVENS テレビ東京アニメ公式</a></td></tr>
-						<tr><td><a class="uk-link-muted" href="https://www.youtube.com/channel/UCvP9G8Z5ndG4-GX81vhgmDg" target="_blank">遊戯王OCGチャンネル - YouTube</a></td></tr>
-						<tr><td><a class="uk-link-muted" href="https://www.konami.com/yugioh/" target="_blank">遊戯王総合サイト - Konami</a></td></tr>
-						<tr><td><a class="uk-link-muted" href="https://www.yugioh-card.com/japan/" target="_blank">遊戯王OCG デュエルモンスターズ - Yugioh-Card.com</a></td></tr>
-						<tr><td><a class="uk-link-muted" href="https://yu-gi-oh.jp/" target="_blank">YU-GI-OH.jp</a></td></tr>
-						<tr><td><a class="uk-link-muted" href="https://www.db.yugioh-card.com/yugiohdb/?request_locale=ja&request_device=sp" target="_blank">遊戯王OCGカードデータベース</a></td></tr>
-						<tr><td><a class="uk-link-muted" href="https://www.konami.com/yugioh/duel_links/ja/" target="_blank">遊戯王 デュエルリンクス - Konami</a></td></tr>
-						<tr><td><a class="uk-link-muted" href="https://www.konamistyle.jp/products/list.php?mode=search&category_id=83" target="_blank">遊戯王 - コナミスタイル</a></td></tr>
-						<tr><td><a class="uk-link-muted" href="http://yugioh-wiki.net/" target="_blank">遊戯王カードWiki - トップページ</a></td></tr>
+						<tr><td><a class="uk-link-muted" href="https://www.tv-tokyo.co.jp/anime/yugioh2000/">テレビ東京・あにてれ 遊☆戯☆王 デュエルモンスターズ</a></td></tr>
+						<tr><td><a class="uk-link-muted" href="https://www.tv-tokyo.co.jp/anime/yugioh-dm/">遊☆戯☆王デュエルモンスターズ 20thリマスター テレビ東京アニメ公式</a></td></tr>
+						<tr><td><a class="uk-link-muted" href="https://www.tv-tokyo.co.jp/anime/yugioh2004/">テレビ東京・あにてれ 遊☆戯☆王デュエルモンスターズGX</a></td></tr>
+						<tr><td><a class="uk-link-muted" href="https://www.tv-tokyo.co.jp/anime/yugioh/">遊☆戯☆王ファイブディーズ - テレビ東京</a></td></tr>
+						<tr><td><a class="uk-link-muted" href="https://www.tv-tokyo.co.jp/anime/yugioh-zexal/">遊☆戯☆王ゼアルⅡ：あにてれ：テレビ東京</a></td></tr>
+						<tr><td><a class="uk-link-muted" href="https://www.tv-tokyo.co.jp/anime/yugioh-arcv/">テレビ東京・あにてれ 遊☆戯☆王ARC-V</a></td></tr>
+						<tr><td><a class="uk-link-muted" href="https://www.tv-tokyo.co.jp/anime/yugioh-vrains/">テレビ東京・あにてれ 遊☆戯☆王VRAINS</a></td></tr>
+						<tr><td><a class="uk-link-muted" href="https://www.tv-tokyo.co.jp/anime/yugioh-sevens/">遊☆戯☆王SEVENS テレビ東京アニメ公式</a></td></tr>
+						<tr><td><a class="uk-link-muted" href="https://www.youtube.com/channel/UCvP9G8Z5ndG4-GX81vhgmDg">遊戯王OCGチャンネル - YouTube</a></td></tr>
+						<tr><td><a class="uk-link-muted" href="https://www.konami.com/yugioh/">遊戯王総合サイト - Konami</a></td></tr>
+						<tr><td><a class="uk-link-muted" href="https://www.yugioh-card.com/japan/">遊戯王OCG デュエルモンスターズ - Yugioh-Card.com</a></td></tr>
+						<tr><td><a class="uk-link-muted" href="https://yu-gi-oh.jp/">YU-GI-OH.jp</a></td></tr>
+						<tr><td><a class="uk-link-muted" href="https://www.db.yugioh-card.com/yugiohdb/?request_locale=ja&request_device=sp">遊戯王OCGカードデータベース</a></td></tr>
+						<tr><td><a class="uk-link-muted" href="https://www.konami.com/yugioh/duel_links/ja/">遊戯王 デュエルリンクス - Konami</a></td></tr>
+						<tr><td><a class="uk-link-muted" href="https://www.konamistyle.jp/products/list.php?mode=search&category_id=83">遊戯王 - コナミスタイル</a></td></tr>
+						<tr><td><a class="uk-link-muted" href="http://yugioh-wiki.net/">遊戯王カードWiki - トップページ</a></td></tr>
 					</tbody>
 				</table>
 				<table class="uk-table uk-table-justify uk-table-small uk-table-divider">
@@ -774,12 +774,12 @@ document.addEventListener('DOMContentLoaded', () => {
 						<tr><th>Twitter</th></tr>
 					</thead>
 					<tbody>
-						<tr><td><a class="uk-link-muted" href="https://twitter.com/yugioh_anime" target="_blank">アニメ「遊☆戯☆王」公式</a></td></tr>
-						<tr><td><a class="uk-link-muted" href="https://twitter.com/YuGiOh_OCG_INFO" target="_blank">【公式】遊戯王OCG</a></td></tr>
-						<tr><td><a class="uk-link-muted" href="https://twitter.com/yu_gi_oh_jp" target="_blank">【公式】YU-GI-OH.jp</a></td></tr>
-						<tr><td><a class="uk-link-muted" href="https://twitter.com/YuGiOh_INS_INFO" target="_blank">【公式】遊戯王OCGインストラクター</a></td></tr>
-						<tr><td><a class="uk-link-muted" href="https://twitter.com/YuGiOh_DL_INFO" target="_blank">【公式】遊戯王 デュエルリンクス</a></td></tr>
-						<tr><td><a class="uk-link-muted" href="https://twitter.com/YuGiOh_DL_INFO" target="_blank">【公式】遊戯王レガシー・オブ・ザ・デュエリスト</a></td></tr>
+						<tr><td><a class="uk-link-muted" href="https://twitter.com/yugioh_anime">アニメ「遊☆戯☆王」公式</a></td></tr>
+						<tr><td><a class="uk-link-muted" href="https://twitter.com/YuGiOh_OCG_INFO">【公式】遊戯王OCG</a></td></tr>
+						<tr><td><a class="uk-link-muted" href="https://twitter.com/yu_gi_oh_jp">【公式】YU-GI-OH.jp</a></td></tr>
+						<tr><td><a class="uk-link-muted" href="https://twitter.com/YuGiOh_INS_INFO">【公式】遊戯王OCGインストラクター</a></td></tr>
+						<tr><td><a class="uk-link-muted" href="https://twitter.com/YuGiOh_DL_INFO">【公式】遊戯王 デュエルリンクス</a></td></tr>
+						<tr><td><a class="uk-link-muted" href="https://twitter.com/YuGiOh_DL_INFO">【公式】遊戯王レガシー・オブ・ザ・デュエリスト</a></td></tr>
 					</tbody>
 				</table>
 			</div>
