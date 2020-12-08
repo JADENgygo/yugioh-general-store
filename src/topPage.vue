d8848a3ae0378f45c59f33f7878f2204404c1eb4
JADEN <jaden@JADEN-Mac.local>
2020-12-08 15:46:29 +0900

    文字を英語に修正

--- src/topPage.vue
+++ src/topPage.vue
@@ -3,8 +3,8 @@
 		<div>
 			<p>遊戯王の雑貨屋です。遊戯王の雑貨を置いています。</p>
 			<ul class="uk-list">
-				<li>ツイッター: <a href="https://twitter.com/oJADENo" class="uk-link-muted">@oJADENo</a></li>
-				<li>インスタグラム: <a href="https://www.instagram.com/ojadeno/" class="uk-link-muted">@ojadeno</a></li>
+				<li>Twitter: <a href="https://twitter.com/oJADENo" class="uk-link-muted">@oJADENo</a></li>
+				<li>Instagram: <a href="https://www.instagram.com/ojadeno/" class="uk-link-muted">@ojadeno</a></li>
 			</ul>
 		</div>
 	</div>
